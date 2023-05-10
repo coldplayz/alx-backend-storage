4d3eb11897c62425a3c035febdc3f96e324183ef
greenbel <obisann@gmail.com>
2023-05-10 18:17:20 +0000

    0x02: implemented a method decorator for calls cnt.

--- 0x02-redis_basic/bkup-exercise.py
+++ 0x02-redis_basic/bkup-exercise.py
@@ -3,7 +3,7 @@
 '''
 import redis
 from uuid import uuid4
-from typing import Union, Callable, Any
+from typing import Union, Callable, Any, Optional
 
 
 class Cache():
@@ -29,7 +29,7 @@ class Cache():
         self._redis.set(rand_key, data)
         return rand_key
 
-    def get(self, key: str, fn: Callable) -> Any:
+    def get(self, key: str, fn: Optional[Callable] = None) -> Any:
         ''' Convert bytes data back to the desired format.
 
         Takes a key string argument and an optional Callable argument named fn
