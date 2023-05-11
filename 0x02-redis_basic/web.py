42e649c445970f70bd87dc5d69cc01762da57e72
greenbel <obisann@gmail.com>
2023-05-11 03:49:40 +0000

    0x02-T5: kept the cache in memory with nx option.

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-create a web cach
+create a web cache
 """
 import redis
 import requests
@@ -11,11 +11,19 @@ client = redis.Redis()
 def get_page(url: str) -> str:
     """ get a page and cache value"""
     resp = requests.get(url)
+    key = "count:{}".format(url)
+    '''
     if client.ttl("count:{}".format(url)) >= 0:
         client.incr("count:{}".format(url))
     else:
         client.set("count:{}".format(url), 0, ex=10)
         client.incr("count:{}".format(url))
+    '''
+    if client.exists(key):
+        if client.ttl(key) >= 0:
+            client.incr(key)
+    else:
+        client.set(key, 1, ex=10, nx=True)
     return resp.text
 
 
