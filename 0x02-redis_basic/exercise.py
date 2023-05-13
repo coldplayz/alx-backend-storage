504afefb374112e4780526fdae20a4d623d0206f
greenbel <obisann@gmail.com>
2023-05-13 12:48:58 +0000

    0x02: testing

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -59,8 +59,8 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()  # clear database
 
-    @call_history
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         ''' Generates a random key (e.g. using uuid),
         store the input data in Redis using the random key and return the key.
